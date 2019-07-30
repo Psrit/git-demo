021f655f78dd6d7936815fb1975744387dfbeb5e
psrit <xiaojx13@outlook.com>
2019-07-30 11:37:35 +0800

    Add test for `fib`.

--- fib.py
+++ fib.py
@@ -1,5 +1,18 @@
 def fib(n: int) -> int:
-    if n == 1:
+    if n == 0:
+        return 0
+    elif n == 1:
         return 1
+    elif isinstance(n, int) and n >= 2:
+        return fib(n - 1) + fib(n - 2)
+    else:
+        raise ValueError(
+            "Invalid argument n: {}. ".format(n) +
+            "Non-negative integer is expected."
+        )
 
-    return fib(n - 1) + fib(n - 2)
+
+if __name__ == "__main__":
+    fib_list = [0, 1, 1, 2, 3, 5, 8, 13]
+    for i, _fib in enumerate(fib_list):
+        assert _fib == fib(i)
