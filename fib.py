4d8cebf86862bf5f7a13f56aea0544c0f101b106
psrit <xiaojx13@outlook.com>
2019-07-30 10:51:38 +0800

    Make recursive `fib` more robust.

--- fib.py
+++ fib.py
@@ -1,5 +1,12 @@
 def fib(n: int) -> int:
     if n == 1:
         return 1
-
-    return fib(n - 1) + fib(n - 2)
+    elif n == 0:
+        return 0
+    elif isinstance(n, int) and n >= 2:
+        return fib(n - 1) + fib(n - 2)
+    else:
+        raise ValueError(
+            "Invalid argument n: {}. ".format(n) +
+            "Non-negative integer is expected."
+        )
